0c562bb9f2b4e5a660af879c60dc5fe1554ee004
dPiazuelo <DAM1@aula11-pc27>
2025-05-12 12:33:06 +0200

    estructura del main terminada

--- Reto3ErnestoDavid/src/Main/main.java
+++ Reto3ErnestoDavid/src/Main/main.java
@@ -9,7 +9,7 @@ public class main {
 		menuPrincipal(sc);
 	}
 	public static void menuPrincipal(Scanner sc) {
-		int opcion=sc.nextInt();
+		int opcion=Util.funciones.dimeEntero("Elige una opcion", sc);
 		do {
 			if(opcion==5) {
 				break;
@@ -17,11 +17,36 @@ public class main {
 				mantenimientos(sc);
 			}else if(opcion==2) {
 				catalogo_prod(sc);
+			}else if(opcion==3) {
+				pedidos(sc);
+			}else if(opcion==4) {
+				informes(sc);
 			}
 		}while(true);
 	}
+	//submenu de informes(opcion 4)
+	public static void informes(Scanner sc) {
+		int opcion=Util.funciones.dimeEntero("Elige una opcion", sc);
+		if(opcion==1) {
+			
+		}else if(opcion==2) {
+			
+		}else if(opcion==3) {
+			
+		}
+	}
+	//submenu de pedidos(opcion 3)
+	public static void pedidos(Scanner sc) {
+		int opcion=Util.funciones.dimeEntero("Elige una opcion", sc);
+		if(opcion==1) {
+			
+		}else if(opcion==2) {
+			
+		}
+	}
+	//submenu de catalogo(opcion 2)
 	public static void catalogo_prod(Scanner sc) {
-		int opcion=sc.nextInt();
+		int opcion=Util.funciones.dimeEntero("Elige una opcion", sc);
 		do {
 			if(opcion==3) {
 				break;
@@ -33,8 +58,9 @@ public class main {
 			}
 		}while(true);
 	}
+	//submenu de mantenimientos(opcion 1)
 	public static void mantenimientos(Scanner sc) {
-		int opcion=sc.nextInt();
+		int opcion=Util.funciones.dimeEntero("Elige una opcion", sc);
 		do {
 			if(opcion==4){
 				break;
@@ -48,7 +74,7 @@ public class main {
 		}while(true);
 	}
 	public static void gestion_clientes(Scanner sc) {
-		int opcion=sc.nextInt();
+		int opcion=Util.funciones.dimeEntero("Elige una opcion", sc);
 		do {
 			if(opcion==3) {
 				break;
