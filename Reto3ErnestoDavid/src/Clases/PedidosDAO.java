99c874fecb59327d155ee7659d821bdefd603b9b
ErnestoRe <ernestorezustaaznarez@gmail.com>
2025-05-13 09:21:47 +0200

    PedidosDAO actualizacion mostrar por mes

--- Reto3ErnestoDavid/src/Clases/PedidosDAO.java
+++ Reto3ErnestoDavid/src/Clases/PedidosDAO.java
@@ -2,22 +2,32 @@ package Clases;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 import Util.Conexion;
 
 public class PedidosDAO {
 
-	public static void mostrarPedFech() {
+	public static List<Pedidos> mostrarPedFech() {
+		List<Pedidos> listaPedFech = new ArrayList<Pedidos>();
 		try (Connection con = Conexion.abreConexion()) {
 			PreparedStatement stmt = con.prepareStatement("select * from pedidos "
 					+ "where month(fecha)=?");
 			int mes = LocalDate.now().getMonthValue();
 			stmt.setInt(1, mes);
-			stmt.execute();
+			ResultSet rs = stmt.executeQuery("select * from categorias");
+			while (rs.next()) {
+				listaPedFech.add(new Pedidos(rs.getInt("idpedido"), rs.getInt("idcliente"), rs.getDouble("precioTotal"), rs.getString("direccionEnvio"), rs.getDate("fecha")));
+			}
+			rs.close();
 		} catch (Exception ex) { 
 			ex.printStackTrace();
 		}finally {
 			Conexion.cierraConexion();
 		}
+		return listaPedFech;
 	}
 }
